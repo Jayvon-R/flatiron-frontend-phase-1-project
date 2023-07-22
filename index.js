9829bb3c7437e70fe78e094499fcb0919a4078c5
Jayvon Rhines <jayvonrhines0@gmail.com>
2023-07-22 01:04:11 -0500

    fetch poke data

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-// Function to fetch Pokémon data from the PokeAPI
+// This function fetches data for 807 Pokémon from the PokeAPI and returns an array with information about each Pokémon.
 async function fetchPokemonData() {
     try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=807`);
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=807`);
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      // Parse the response data as JSON, so we can read and use the data.
+      const data = await response.json();
+      // Extract the list of Pokémon from the data.
+      const pokemonList = data.results;
+      const pokemonData = [];
+      for (const pokemon of pokemonList) {
+        const res = await fetch(pokemon.url);
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
         }
-        const data = await response.json();
-        const pokemonList = data.results;
-        
-        const pokemonData = [];
-        for (const pokemon of pokemonList) {
-            const res = await fetch(pokemon.url);
-            if (!res.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const pokemonInfo = await res.json();
-            pokemonData.push(pokemonInfo);
-        }
-        
-        return pokemonData;
+        // Parse the response data as JSON to get detailed information about each Pok emon.
+        const pokemonInfo = await res.json();
+        pokemonData.push(pokemonInfo);
+      }
+      return pokemonData;
     } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error; 
+      console.error('Error fetching data:', error);
+      throw error;
     }
-}
+  }
+
+  // Event listener to fetch data and display Pokemon cards on page load
+document.addEventListener('DOMContentLoaded', async () => {
+    const pokemonData = await fetchPokemonData();
+    displayPokemonCards(pokemonData);
+});
+  
